01b4bcb4dbe29449daebf334861e95549c00e488
SangWon7242 <gotkdqja@gmail.com>
2025-01-21 19:38:49 +0900

    ArticleService, ArticleRepository 도입

--- src/main/java/com/sbs/jsp/board/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/sbs/jsp/board/boundedContext/article/controller/ArticleController.java
@@ -2,39 +2,25 @@ package com.sbs.jsp.board.boundedContext.article.controller;
 
 import com.sbs.jsp.board.base.Rq;
 import com.sbs.jsp.board.boundedContext.article.dto.Article;
+import com.sbs.jsp.board.boundedContext.article.service.ArticleService;
+import com.sbs.jsp.board.container.Container;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ArticleController {
-  private List<Article> articleList;
+  private ArticleService articleService;
 
   public ArticleController() {
-    articleList = new ArrayList<>();
-    makeTestData();
-  }
-
-  private void makeTestData() {
-    // 스트림 문법
-    IntStream.rangeClosed(1, 5)
-        .forEach(i ->
-            articleList.add(new Article(i, "제목 %d".formatted(i), "내용 %d".formatted(i)))
-        );
+    articleService = Container.articleService;
   }
 
   public void showList(Rq rq) {
-    // articleList : 정렬되지 않은 1 ~ 5까지의 게시물 리스트
+    List<Article> articles = articleService.getArticles();
 
-    // a1, a2 : article 객체
-    // Integer.compare : 두 정수를 비교하는 메서드
-    // - a < b : 음수 반환
-    // - a == b : 0을 반환
-    // - a > b : 양수 반환
-    // Long.compare(a2.getId(), a1.getId()) : 둘을 비교해서 내림차순 정렬
-    List<Article> articles = articleList.stream()
-        .sorted((a1, a2) -> Long.compare(a2.getId(), a1.getId()))
-        .toList(); // stream의 결과를 list로 변환
+    if(articles.isEmpty()) {
+      rq.print("게시물이 존재하지 않습니다.");
+      return;
+    }
 
     rq.setAttr("articles", articles);
 
@@ -60,7 +46,8 @@ public class ArticleController {
       return;
     }
 
-    rq.print("<div>subject : %s</div>\n".formatted(subject));
-    rq.print("<div>content : %s</div>\n".formatted(content));
+    long id = articleService.write(subject, content);
+
+    rq.print("<div>%d번 게시물이 생성되었습니다.</div>\n".formatted(id));
   }
 }
